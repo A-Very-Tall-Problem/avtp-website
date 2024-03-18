6132e8b3ddc27090ddff33bbfb31e92ac763b489
noxeorn <noxscha@gmail.com>
2024-03-18 19:22:00 +0300

    Slider script

---
+++ restaurant/js/slider.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+jQuery('.slider').slick({
+    dots: false,      
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    speed: 800,      
+    cssEase: 'linear'
+  });     });
